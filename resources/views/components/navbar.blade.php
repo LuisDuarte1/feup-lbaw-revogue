4360d0dca6f6528168d6fc4f07fb847dddb1c3e4
Madalena Ye <mads1040@gmail.com>
2023-11-18 19:21:17 +0000

    navbar prototype

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar">
+    <div class="logo">
+        <a href="#">
+            <h1> ReVogue </h1>
+        </a>
+    </div>
+    <ul class="menu">
+        <li>
+            <a href="#">
+                <ion-icon name="person-circle"></ion-icon>
+                    Profile
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                    <ion-icon name="home"></ion-icon>
+                    Home
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                    <ion-icon name="bag-handle-sharp"></ion-icon>
+                    Listings
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                    <ion-icon name="chatbubble-ellipses-sharp"></ion-icon>
+                    Messages
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                    <ion-icon name="settings"></ion-icon>
+                    Settings
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                    <ion-icon name="help-circle"></ion-icon>
+                    FAQs
+            </a>
+        </li>
+        <li>
+            <a href="#" class="logout">
+                    <ion-icon name="log-out"></ion-icon>
+                    Logout
+            </a>
+        </li>
+    </ul>
+</nav>
