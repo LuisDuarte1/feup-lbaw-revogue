226d7ceec84c3f8c48f5a3025da08badae274b88
rita cachaldora <ritacachaldorapereira@gmail.com>
2023-11-19 19:53:40 +0000

    fixes for product-card

--- app/View/Components/productCard.php
+++ app/View/Components/productCard.php
@@ -1,31 +1,30 @@
 <?php
+
 namespace App\View\Components;
 
-use Closure;
 use Illuminate\View\Component;
-use Illuminate\Contracts\View\View;
 
 class ProductCard extends Component
 {
-   public $image;
-   public $price;
-   public $size;
+    public $image;
 
-   public function __construct($image, $price, $size)
-   {
-       $this->image = $image;
-       $this->price = $price;
-       $this->size = $size;
-   }
+    public $price;
 
-   public function render()
-   {
-       return view('components.productCard', [
-           'image' => $this->image,
-           'price' => $this->price,
-           'size' => $this->size,
-       ]);
-   }
+    public $size;
+
+    public function __construct($image, $price, $size)
+    {
+        $this->image = $image;
+        $this->price = $price;
+        $this->size = $size;
+    }
+
+    public function render()
+    {
+        return view('components.productCard', [
+            'image' => $this->image,
+            'price' => $this->price,
+            'size' => $this->size,
+        ]);
+    }
 }
-
-?>
--- resources/views/components/productCard.blade.php
+++ resources/views/components/productCard.blade.php
@@ -1,15 +1,13 @@
 <div class="product-card">
-   <div class="product-layout">
-        <div class="product-image-card">
-            <img src="{{ $image }}" alt="product image">
+    <div class="product-image-card">
+        <img src="{{ $image }}" alt="product image">
+    </div>
+    <div class="product-description-card">
+        <div class="product-size">
+            <p>{{ $size }}</p>
         </div>
-        <div class="product-description-card">
-            <div class="product-size">
-                <p>{{ $size }}</p>
-            </div>
-            <div class="product-price">
-                <p>{{ $price }}</p>
-            </div>
+        <div class="product-price">
+            <p>{{ $price }}</p>
         </div>
-    <div>
+    </div>
 </div>
