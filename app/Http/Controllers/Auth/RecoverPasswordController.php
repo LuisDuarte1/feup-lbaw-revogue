e2289a4731c67286ea3c07f8e6ef3ecb7b8baed6
TomasdosBeefs <tomaspr123@gmail.com>
2023-11-19 22:24:32 +0000

    Formatted

--- app/Http/Controllers/Auth/RecoverPasswordController.php
+++ app/Http/Controllers/Auth/RecoverPasswordController.php
@@ -5,21 +5,18 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
-class RecoverPasswordController extends Controller 
+class RecoverPasswordController extends Controller
 {
-
     public function showRecoverPasswordForm()
     {
         return view('auth.recover-password');
     }
 
-
     public function sendEmailPasswordRecovery(Request $request): RedirectResponse
     {
         $credentials = $request->validate([
-            'email' => ['required', 'email',],
+            'email' => ['required', 'email'],
         ]);
 
         $email = Password::sendResetLink(
@@ -28,10 +25,7 @@ class RecoverPasswordController extends Controller
 
         return $status === Password::RESET_LINK_SENT
                     ? back()->with(['status' => __($status)])
-                    : back()->withErrors(['email' => __($status)]); 
-
+                    : back()->withErrors(['email' => __($status)]);
 
     }
-
-
 }
