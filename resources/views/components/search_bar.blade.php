0e629994579dd9472547c3a75bb3c373558ad8ef
TomasdosBeefs <tomaspr123@gmail.com>
2023-11-19 18:59:40 +0000

    search bar and favorites and cart icons

---
+++ resources/views/components/search_bar.blade.php
@@ -0,0 +1,13 @@
+
+<nav class= "topnav">
+    <ion-icon id="search_icon" name="search"></ion-icon>
+    <form method="POST" action="#">
+        <input class="search" type="text" placeholder="Search">
+    </form>
+    <a href="#">
+        <ion-icon name="heart-outline"></ion-icon>
+    </a>
+    <a href="#">
+        <ion-icon name="cart"></ion-icon>
+    </a>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
 
     </head>
     <body>
+        <x-search_bar/>
         <x-navbar/>
         <div class="main-content">@yield('content')</div>
     </body>
