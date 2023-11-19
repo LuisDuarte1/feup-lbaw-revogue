4811e9ae8d9de4b144aaab95333da3537a2c8bb4
rita cachaldora <ritacachaldorapereira@gmail.com>
2023-11-19 00:41:07 +0000

    product card full implementation

---
+++ resources/views/components/productCard.blade.php
@@ -0,0 +1,15 @@
+<div class="product-card">
+   <div class="product-layout">
+        <div class="product-image-card">
+            <img src="{{ $image }}" alt="product image">
+        </div>
+        <div class="product-description-card">
+            <div class="product-size">
+                <p>{{ $size }}</p>
+            </div>
+            <div class="product-price">
+                <p>{{ $price }}</p>
+            </div>
+        </div>
+    <div>
+</div>
