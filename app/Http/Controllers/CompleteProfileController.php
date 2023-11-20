b3c6954e56012a89d3c80c522b774d452f5a9713
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-11-20 12:22:33 +0000

    complete profile backend

--- app/Http/Controllers/CompleteProfileController.php
+++ app/Http/Controllers/CompleteProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CompleteProfileController extends Controller
 {
@@ -11,6 +12,24 @@ class CompleteProfileController extends Controller
     }
 
     function postProfile(Request $request){
+        $validated = $request->validate([
+            'bio' => "nullable|string|max:10000",
+            'profileImage' => "nullable|image|max:5000"
+        ]);
         
+        $user = $request->user();
+
+        if($request->filled('bio') && $validated['bio']){
+            $user->update(['bio' => $validated['bio']]);
+        }
+        if($request->hasFile('profileImage')){
+            if(isset($request->user()->profile_image_path)) {
+                Storage::disk('public')->delete($request->user()->profile_image_path);
+            }
+            $filename = $user->id.'-'.$request->file('profileImage')->getClientOriginalName();
+            $path = $request->file('profileImage')->storeAs('avatars', $filename, "public");
+            $user->update(['profile_image_path' => $path]);
+        }
+        return redirect('/');
     }
 }
--- resources/views/pages/completeProfile.blade.php
+++ resources/views/pages/completeProfile.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="page-center">
-    <div class="complete-profile-page">
-        <h1 class="title">Complete Profile</h1>
-        <p>Thanks for verifying your email! You should now complete you registration by upload a photo and writing a bit about yourself so that other users can know a little about you.<br><br> If you don't feel like doing this you can do it later by accessing the settings page. Welcome to ReVougue!</p>
-        <form class="complete-profile-form" action="{{ route('complete-profile') }}" method="POST">
-            <div class="complete-profile-items">
-                <div class="select-image">
-                    <img id="image" src="{{$imagePath}}">
-                    <input type="file" id="profileImage" name="profileImage" hidden>
-                    <label for="profileImage">Upload photo</label>
+    <div class="page-center">
+        <div class="complete-profile-page">
+            <h1 class="title">Complete Profile</h1>
+            <p>Thanks for verifying your email! You should now complete you registration by uploading a photo and writing a
+                bit about yourself so that other users can know a little about you.<br><br> If you don't feel like doing
+                this you can do it later by accessing the settings page. Welcome to ReVogue!</p>
+            <form class="complete-profile-form" action="{{ route('complete-profile') }}" method="POST" enctype='multipart/form-data'>
+                @csrf
+                <div class="complete-profile-items">
+                    <div class="select-image">
+                        <img id="image" src="/storage/{{ $imagePath }}">
+                        <input type="file" id="profileImage" name="profileImage" hidden>
+                        <label for="profileImage">Upload photo</label>
+                    </div>
+                    <div class="biography">
+                        <label for="bio">Biography:</label>
+                        <textarea id="bio" name="bio" placeholder="Tell me about yourself..."></textarea>
+                    </div>
                 </div>
-                <div class="biography">
-                    <label for="bio">Biography:</label>
-                    <textarea id="bio" name="bio" placeholder="Tell me about yourself..."></textarea>
-                </div>
-            </div>
-            <button type="submit">Update profile</button>
-        </form>
+                <button type="submit">Update profile</button>
+            </form>
+        </div>
     </div>
-</div>
-
 @endsection
