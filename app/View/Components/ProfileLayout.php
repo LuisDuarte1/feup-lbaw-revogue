80fc3e2ac4fd76ef71d81f857732c46c99ab98b4
rita cachaldora <ritacachaldorapereira@gmail.com>
2023-11-20 17:50:34 +0000

    profile layout blade view/component

---
+++ app/View/Components/ProfileLayout.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class ProfileLayout extends Component
+{
+    public $profilePicture;
+
+    public $name;	
+    
+    public $username;
+
+    public $bio;
+
+    public $rating;
+
+    public 
+    public function __construct($profilePicture, $name, $username, $bio, $rating)
+    {
+        $this->profilePicture = $profilePicture;
+        $this->name = $name;
+        $this->username = $username;
+        $this->bio = $bio;
+        $this->rating = $rating;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.profile-layout',[
+            'profilePicture' => $this->profilePicture,
+            'name' => $this->name,
+            'username' => $this->username,
+            'bio' => $this->bio,
+            'rating' => $this->rating,
+        ]);
+    }
+}
