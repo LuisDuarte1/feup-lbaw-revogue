0ee2ca652008006c3ae70e62ef4c5ac17f34cbff
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-11-27 09:53:00 +0000

    defer product cart image

--- resources/views/components/productCard.blade.php
+++ resources/views/components/productCard.blade.php
@@ -2,7 +2,7 @@
     <div class="product-card">
         <div class="product-image-card">
             <!--TODO (luisd): do the random hash if only on debug mode -->
-            <img src="{{ $image }}?hash={{fake()->lexify('???????????????')}}" alt="product image">
+            <img src="{{ $image }}?hash={{fake()->lexify('???????????????')}}" loading="lazy" decoding="async" alt="product image">
         </div>
         <div class="product-description-card">
             <div class="product-size">
