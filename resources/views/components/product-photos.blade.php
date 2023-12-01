2571e1ccc32f8f0f4721b90e5103b7e9eb748246
Madalena Ye <mads1040@gmail.com>
2023-12-01 16:10:59 +0000

    extracting component for product photos

---
+++ resources/views/components/product-photos.blade.php
@@ -0,0 +1,16 @@
+<div class="upload-photos">
+    <div class="add-photo">
+        <input type="file" id="product-photos" name="imageToUpload" multiple>
+        <label for="product-photos" class="product-photos">
+            <ion-icon name="camera"></ion-icon>
+            Add photos
+        </label>
+    </div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+    <div class="add-photo"></div>
+</div>
