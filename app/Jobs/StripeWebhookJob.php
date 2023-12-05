a5e927fe5522bc50ef581af40e18b758119d4db4
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-12-05 11:35:47 +0000

    make stripe webhook logic

--- app/Jobs/StripeWebhookJob.php
+++ app/Jobs/StripeWebhookJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Purchase;
 use App\Models\PurchaseIntent;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -9,6 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class StripeWebhookJob implements ShouldQueue
 {
@@ -23,7 +25,37 @@ class StripeWebhookJob implements ShouldQueue
         if(!isset($paymentIntent->id)){
             return;
         }
+        $cart = $purchaseIntent->products()->get();
+        $cartGrouped = $purchaseIntent->products()->groupBy('sold_by');
+        DB::beginTransaction();
+        $purchase = Purchase::create(['method' => 'stripe']);
+        foreach ($cartGrouped as $soldBy => $products) {
+            $order = $purchaseIntent->user()->orders()->create([
+                'status' => 'pendingShipment', //payment at delivery goes straight to pendingShipment
+                'shipping_address' => [
+                    'name' => $purchaseIntent->shipping_address->full_name,
+                    'email' => $purchaseIntent->shipping_address->email,
+                    'country' => $purchaseIntent->shipping_address->country,
+                    'address' => $purchaseIntent->shipping_address->address,
+                    'zip-code' => $purchaseIntent->shipping_address->zip_code,
+                    'phone' => $purchaseIntent->shipping_address->phone,
+                ],
+                'purchase' => $purchase->id,
+            ]);
 
+            $ids = [];
+            foreach ($products as $product) {
+                array_push($ids, $product->id);
+            }
+            $order->products()->attach($ids);
+        }
+        // TODO(luisd): send notification
+        foreach ($cart as $product) {
+            $product->inCart()->detach();
+            $product->wishlist()->detach();
+        }
+        $purchaseIntent->delete();
+        DB::commit();
     }
 
     /**
