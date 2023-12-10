f40f050ed0d6636f559f41308a85711d0ea9feff
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-12-10 17:28:39 +0000

    make notification return in creation desc order

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -22,7 +22,7 @@ class NotificationController extends Controller
     function getNotificationsAPI(Request $request){
         $user = $request->user();
 
-        $notifications = $user->notifications()->where('dismissed', 'false')->paginate(10)->withQueryString();
+        $notifications = $user->notifications()->where('dismissed', 'false')->orderBy('creation_date', 'DESC')->paginate(10)->withQueryString();
         if($notifications->isEmpty()){
             return response('',204);
         }
