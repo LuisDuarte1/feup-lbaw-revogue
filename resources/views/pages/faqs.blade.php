0f467757074b19a79d04c5304ad80ac50deeb145
rita cachaldora <ritacachaldorapereira@gmail.com>
2023-12-13 06:03:00 +0000

    faq page css

--- resources/views/pages/faqs.blade.php
+++ resources/views/pages/faqs.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app', ['search_bar' => true])
 
-
 @section('content')
-<div class="login-wrapper">
-    <div class="login-page">
-        <h1 class="title">FAQs</h1>
-        <section class="question-category">
-            <h2>Orders</h2>
-            <div class="question-container">
-                <div class="question">
-                    <h3>How can I contact you?</h3>
-                    <p>You can contact us by sending an <a href="mailto:revogue021@gmail.com">email</a></p>
-                </div>
+<div class="faqs-wrapper">
+    <div class="faqs-page">
+        <h1 class="title">FAQ's</h1>
+        <div class="questions-wrapper">     
+            <div class="question-category">
+                <h2>Orders</h2>
+                    <div class="question-box">
+                        <button class="question">How can I contact you?</button>
+                        <div class="answer">
+                            <p>You can contact us by sending an <a href="mailto:revogue021@gmail.com">email</a></p>
+                        </div>
+                    </div>
             </div>
-        </section>
+        </div>
     </div>
 </div>
 @endsection
