5df85122c103f964e4a6bea179a91aeb1b7bbee8
Madalena Ye <mads1040@gmail.com>
2023-12-14 23:02:40 +0000

    resolving conflicts

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -23,7 +23,7 @@
         $size = $product['size'];
         $id = $product['product']->id;
         @endphp
-        <x-productCard :price="$price" :image="$image" :size="$size" :id="$id" />
+        <x-productCard :price="$price" :image="$image" :size="$size" :id="$id" :shipping="2" :condition="'Good'" />
         @endforeach
     </div>
     <div id="page-end">
