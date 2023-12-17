93cd931f084d87c3ccc69a52ec9f34f34e82a503
Madalena Ye <mads1040@gmail.com>
2023-12-17 19:33:00 +0000

    fixing spaghetti and added margin to navbar

--- resources/views/components/profileLayout.blade.php
+++ resources/views/components/profileLayout.blade.php
@@ -3,21 +3,23 @@
         <img src="{{ $profilePicture }}" alt="profile picture">
     </div>
     <div class="profile-description">
-        <div class="profile-descriptiom-top">
-            <div class="profile-name">
-                <h1>{{ $name }}</h1>
-            </div>
-            <div class="profile-username">
-                <h2> {{'@' . $username }}</h2>
+        <div class="profile-description-top column">
+            <div class="profile-user">
+                <div class="profile-name">
+                    <h1>{{ $name }}</h1>
+                </div>
+                <div class="profile-username">
+                    <h2> {{'@' . $username }}</h2>
+                </div>
             </div>
             <div class="rating">
-                <x-reviewStars :rating="$rating" />                
+                <x-reviewStars :rating="$rating" />
                 <a href="/profile/{{ $id }}/reviews">({{ round($rating,1) }})</a>
             </div>
         </div>
         <div class="profile-description-bottom">
             <div class="profile-bio">
-                <p>{{ $bio }}</p> 
+                <p>{{ $bio }}</p>
             </div>
         </div>
     </div>
