053a038fbc6af42a4e0299b4958786e386e3de7c
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-12-18 21:34:57 +0000

    adjust queue worker params

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,6 @@ cd /var/www
 env >> /var/www/.env
 php artisan clear-compiled
 php artisan config:clear
-php artisan queue:work &
+php artisan queue:work --tries=1 &
 php-fpm8.1 -D
 nginx -g "daemon off;"
