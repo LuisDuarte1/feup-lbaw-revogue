728fbdb44ad0a6bd366a695fa34863d8aa13b7a9
Madalena Ye <mads1040@gmail.com>
2023-12-18 03:31:52 +0000

    cart products redesign

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -3,54 +3,56 @@
 @section('content')
 
 <section class="layout-wrap">
-    <div class="shopping-cart column gap-1 justify-center">
+    <div class="shopping-cart column gap-2 justify-center">
         <div class="cart-header line">
-            <div class="cart-title">
-                <h1 class="title">Shopping Cart</h1>
+            <h1 class="title">Shopping Cart</h1>
+        </div>
+        <div class="purchase-details row items-center">
+            <p>Purchase details</p>
+            <p># Orders</p>
+        </div>
+        <div class="cart-orders column gap-3">
+            <div class="seller-products column gap-2">
+                <div class="seller-products-header row items-center">
+                    <div class="row wrapper items-center">
+                        <img src="/defaultProfileImage.png" alt="seller" class="seller-image">
+                        <div class="selller-name">
+                            <div class="seller-display-name">Display name</div>
+                            <div class="seller-username">@username</div>
+                        </div>
+                    </div>
+                    <a href="#" class="cart-order-remove">
+                        <button>Remove All</button>
+                    </a>
+                </div>
+                <div class="cart-products column gap-1">
+                    <article class="product column gap-1">
+                        <a href="#" class="product-remove"><ion-icon name="close-outline"></ion-icon></a>
+                        <div class="cart-product row gap-1 items-center">
+                            <div class="product-image column items-center">
+                                <a href="#">
+                                    <img src="https://picsum.photos/1020/720">
+                                </a>
+                            </div>
+                            <div class="cart-product-info row gap-1">
+                                <a href="#" class="column wrapper">
+                                    <div class="product-name">Product Name</div>
+                                    <div class="product-size">Size: M</div>
+                                    <div class="product-condition">Condition: Good</div>
+                                </a>
+                                <div class="product-price">Price: 20€</div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
             </div>
         </div>
-        <div class="product-categories">
-            <p>Product details</p>
-        </div>
-        <div class="products column gap-1">
-            @foreach ($cart as $product)
-                @php
-                    $image = $product['product']->image_paths[0];
-                    $title = $product['product']->name;
-                    $id = $product['product']->id;
-                    $color = $product['color'];
-                    $size = $product['size'];
-                    $price = $product['product']->price;
-
-                @endphp
-                <article class="product line" data-id="{{$id}}">
-                    <div class="layout-divider image-details-group">
-                        <div class="product-image">
-                            <img src="{{$image}}" alt="product">
-                        </div>
-                        <div class="product-details">
-                            <h3>{{$title}}</h3>
-                            <p>Color: <span>{{$color}}</span></p>
-                            <p>Size: <span>{{$size}}</span></p>
-                        </div>
-                    </div>
-                    <div class="layout-divider price-trash-group">
-                        <div class="product-price">
-                            <p>{{$price}}</p>
-                        </div>
-                        <div class="product-remove">
-                            <a href="#"><ion-icon name="trash"></ion-icon></a>
-                        </div>
-                    </div>
-                </article>
-            @endforeach
-        </div>
     </div>
-    <div class="order-summary column gap-2">
-        <div class="order-title line">
-            <h1 class="title">Order Summary</h1>
+    <div class="purchase-summary column gap-2">
+        <div class="purchase-title line">
+            <h1 class="title">Purchase Summary</h1>
         </div>
-        <div class="order-categories">
+        <div class="purchase-details items-center row">
             <p>{{count($cart)}} items</p>
             <p>{{$sum}}€</p>
         </div>
@@ -62,13 +64,13 @@
         </div>
         <form action="">
             <div class="promo column justify-center gap-1">
-                    <h3><label for="promo-code">
+                <h3><label for="promo-code">
                         Promo code:
                     </label></h3>
-                    <div class="apply-button">
+                <div class="apply-button">
                     <input type="text" id="promo-code" name="promo-code">
-                        <button>Apply</button>
-                    </div>
+                    <button>Apply</button>
+                </div>
             </div>
         </form>
         <span class="line"></span>
