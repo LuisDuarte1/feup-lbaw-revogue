b8b69f466515166663a63873fbd38f134e05daed
Madalena Ye <mads1040@gmail.com>
2023-12-20 21:30:31 +0000

    lint fix

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
-
-
 class SettingsController extends Controller
 {
     public function getPaymentSettings()
