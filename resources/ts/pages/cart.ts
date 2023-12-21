aa9d0ed935ea618c4a13df0b15d6c822987c66a0
TomasdosBeefs <tomaspr123@gmail.com>
2023-12-21 15:04:16 +0000

    finnished the cart bugs

--- resources/ts/pages/cart.ts
+++ resources/ts/pages/cart.ts
@@ -1,6 +1,37 @@
-function deleteProductHandler (parent: Element): (event: Event) => Promise<void> {
-  return async (event: Event) => {
+function updateSum (dataPrice: number): void {
+  const sumElements = document.querySelectorAll('.price-sum')
+  sumElements.forEach((sumElement) => {
+    const currentSum = Number.parseFloat(sumElement.textContent ?? '0')
+    sumElement.textContent = ((currentSum - dataPrice)).toFixed(2).toString()
+  })
+}
+
+function updateNumberOfProducts (): void {
+  const numberOfProducts = document.querySelectorAll('.product').length
+  const numberOfProductsElement = document.querySelector('.number-of-products')
+  if (numberOfProductsElement !== null) {
+    numberOfProductsElement.textContent = numberOfProductsElement.textContent?.replace(/\d+/, numberOfProducts.toString()) ?? null
+  }
+}
+
+function removeProductAndSellerDiv (parent: Element): void {
+  const sellerProductsDiv = parent.closest('.seller-products')
+  const orders = document.querySelector('.number-of-orders')
+  parent.remove()
+  if (sellerProductsDiv !== null) {
+    if (sellerProductsDiv.querySelector('.product') === null) {
+      sellerProductsDiv.remove()
+      if (orders !== null) {
+        const numberOfOrders = Number.parseInt(orders.textContent ?? '0')
+        orders.textContent = orders.textContent?.replace(/\d+/, (numberOfOrders - 1).toString()) ?? null
+      }
+    }
+  }
+}
+function deleteProductHandler (parent: Element): (event?: Event) => Promise<void> {
+  return async (event?: Event) => {
     const dataId = Number.parseInt(parent.attributes.getNamedItem('data-id')?.value ?? '-1')
+    const dataPrice = Number.parseFloat(parent.attributes.getNamedItem('data-price')?.value ?? '-1')
     if (dataId === -1) {
       return
     }
@@ -17,9 +48,11 @@ function deleteProductHandler (parent: Element): (event: Event) => Promise<void>
     if (req.status === 404) {
       return
     }
-    parent.remove()
     // TODO(luisd): insert empty message when removing🚬
     // TODO(luisd): update count and total price🚬
+    removeProductAndSellerDiv(parent)
+    updateSum(dataPrice)
+    updateNumberOfProducts()
   }
 }
 
@@ -34,7 +67,23 @@ function deleteProduct (): void {
   })
 }
 
+function deleteAllProductsFromSellerButton (): void {
+  const buttons = document.querySelectorAll('.remove-all-button')
+  buttons.forEach((button) => {
+    button.addEventListener('click', async () => {
+      const sellerId = button.getAttribute('data-seller-id')
+      const sellerProductsDiv = document.querySelector(`.seller-products[data-seller-id="${sellerId}"]`)
+      if (sellerProductsDiv !== null) {
+        const products = sellerProductsDiv.querySelectorAll('.product')
+        // eslint-disable-next-line @typescript-eslint/promise-function-async
+        const promises = Array.from(products).map((product) => deleteProductHandler(product)(undefined))
+        await Promise.all(promises)
+      }
+    })
+  })
+}
 export function cart (): void {
   window.history.replaceState(null, '', window.location.pathname)
   deleteProduct()
+  deleteAllProductsFromSellerButton()
 }
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -1,118 +1,126 @@
 @extends('layouts.app')
 
 @section('content')
-@php
-$sum = 0;
-$numOrders = count($cart);
-$numProducts = 0;
-@endphp
-<section class="layout-wrap">
-    <div class="shopping-cart column gap-2 justify-center">
-        <div class="cart-header line">
-            <h1 class="title">Shopping Cart</h1>
-        </div>
-        <div class="purchase-details row items-center">
-            <p>Purchase details</p>
-            <p>{{$numOrders}} Orders</p>
-        </div>
-        <div class="cart-orders column gap-3">
-            @foreach($cart as $id => $products)
-            @php
-            $numProducts += count($products);
-            $seller = App\Models\User::where('id', $id)->get()->first();
-            $sellerPicture = $seller->profile_image_path !== null ? "/storage/".$user->profile_image_path : '/defaultProfileImage.png';
-            @endphp
-            <div class="seller-products column gap-2">
-                <div class="seller-products-header row items-center">
-                    <div class="row wrapper items-center">
-                        <img src="{{$sellerPicture}}" alt="seller" class="seller-image">
-                        <div class="selller-name">
-                            <a href="/profile/{{$seller->id}}" class="profile-link">
-                                <div class="seller-display-name">{{$seller->display_name}}</div>
-                                <div class="seller-username">{{'@' . $seller->username}}</div>
+    @php
+        $sum = 0;
+        $numOrders = count($cart);
+        $numProducts = 0;
+    @endphp
+    <section class="layout-wrap">
+        <div class="shopping-cart column gap-2 justify-center">
+            <div class="cart-header line">
+                <h1 class="title">Shopping Cart</h1>
+            </div>
+            <div class="purchase-details row items-center">
+                <p>Purchase details</p>
+                <p class="number-of-orders">{{ $numOrders }} Orders</p>
+            </div>
+            <div class="cart-orders column gap-3">
+                @foreach ($cart as $id => $products)
+                    @php
+                        $numProducts += count($products);
+                        $seller = App\Models\User::where('id', $id)
+                            ->get()
+                            ->first();
+                        $sellerPicture = $seller->profile_image_path !== null ? '/storage/' . $user->profile_image_path : '/defaultProfileImage.png';
+                    @endphp
+                    <div class="seller-products column gap-2" data-seller-id="{{ $seller->id }}"">
+                        <div class="seller-products-header row items-center">
+                            <div class="row wrapper items-center">
+                                <img src="{{ $sellerPicture }}" alt="seller" class="seller-image">
+                                <div class="selller-name">
+                                    <a href="/profile/{{ $seller->id }}" class="profile-link">
+                                        <div class="seller-display-name">{{ $seller->display_name }}</div>
+                                        <div class="seller-username">{{ '@' . $seller->username }}</div>
+                                    </a>
+                                </div>
+                            </div>
+                            <a href="#" class="cart-order-remove">
+                                <button class="remove-all-button" data-seller-id="{{ $seller->id }}">Remove All</button>
                             </a>
                         </div>
-                    </div>
-                    <a href="#" class="cart-order-remove">
-                        <button>Remove All</button>
-                    </a>
-                </div>
-                <div class="cart-products column gap-1">
-                    @foreach($products as $product)
-                    @php
-                    $sum += $product->price;
-                    $productPicture = $product->image_paths[0];
-                    $price = $product->price;
-                    $size = $product->attributes()->where('key', 'Size')->get()->first()->value;
-                    @endphp
-                    <article class="product column gap-1" data-id="{{$product->id}}">
-                        <a href="#" class="product-remove"><ion-icon name="close-outline"></ion-icon></a>
-                        <div class="cart-product row gap-1 items-center">
-                            <div class="product-image column items-center">
-                                <a href="/products/{{$product->id}}">
-                                    <img src="{{$productPicture}}">
-                                </a>
-                            </div>
-                            <div class="cart-product-info row gap-1">
-                                <a href="/products/{{$product->id}}" class="column wrapper">
-                                    <div class="product-name">{{$product->name}}</div>
-                                    <div class="product-size">Size: {{$size}}</div>
-                                    <div class="product-condition">Condition: Good</div>
-                                </a>
-                                <div class="product-price">Price: {{$price}}€</div>
-                            </div>
+                        <div class="cart-products
+                                    column gap-1">
+                            @foreach ($products as $product)
+                                @php
+                                    $sum += $product->price;
+                                    $productPicture = $product->image_paths[0];
+                                    $price = $product->price;
+                                    $size = $product
+                                        ->attributes()
+                                        ->where('key', 'Size')
+                                        ->get()
+                                        ->first()->value;
+                                @endphp
+                                <article class="product column gap-1" data-id="{{ $product->id }}"
+                                    data-price="{{ $price }}">
+                                    <a href="#" class="product-remove"><ion-icon name="close-outline"></ion-icon></a>
+                                    <div class="cart-product row gap-1 items-center">
+                                        <div class="product-image column items-center">
+                                            <a href="/products/{{ $product->id }}">
+                                                <img src="{{ $productPicture }}">
+                                            </a>
+                                        </div>
+                                        <div class="cart-product-info row gap-1">
+                                            <a href="/products/{{ $product->id }}" class="column wrapper">
+                                                <div class="product-name">{{ $product->name }}</div>
+                                                <div class="product-size">Size: {{ $size }}</div>
+                                                <div class="product-condition">Condition: Good</div>
+                                            </a>
+                                            <div class="product-price">Price: {{ $price }}€</div>
+                                        </div>
+                                    </div>
+                                </article>
+                            @endforeach
                         </div>
-                    </article>
-                    @endforeach
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="purchase-summary column gap-2">
+            <div class="purchase-title line">
+                <h1 class="title">Purchase Summary</h1>
+            </div>
+            <div class="purchase-details items-center row">
+                <p class="number-of-products">{{ $numProducts }} items</p>
+                <p class="price-sum">{{ $sum }}€</p>
+            </div>
+            <div class="shipping column justify-center gap-1">
+                <h3>Shipping: </h3>
+                <div class="shipping-price">
+                    <p>Standard Delivery - Free</p>
                 </div>
             </div>
+            <div class="promo column justify-center gap-1">
+                <h3><label for="promo-code">
+                        Promo code:
+                    </label></h3>
+                <div class="apply-button">
+                    <input type="text" id="promo-code" name="promo-code">
+                    <button>Apply</button>
+                </div>
+            </div>
+            <span class="line"></span>
+            @foreach ($appliedVouchers as $voucher)
+                @php
+                    $difference = $voucher->getProduct->price - $voucher->bargainMessage->proposed_price;
+                @endphp
+                <div class="voucher row justify-between">
+                    <p class="voucher-code">Voucher: <span>{{ $voucher->code }}</span><a href="#"
+                            class="voucher-remove" data-voucher-code="{{ $voucher->code }}"><ion-icon
+                                name="close-circle-outline"></ion-icon></a></p>
+                    <p class="difference">-{{ $difference }}€</p>
+                </div>
             @endforeach
-        </div>
-    </div>
-    <div class="purchase-summary column gap-2">
-        <div class="purchase-title line">
-            <h1 class="title">Purchase Summary</h1>
-        </div>
-        <div class="purchase-details items-center row">
-            <p>{{$numProducts}} items</p>
-            <p>{{$sum}}€</p>
-        </div>
-        <div class="shipping column justify-center gap-1">
-            <h3>Shipping: </h3>
-            <div class="shipping-price">
-                <p>Standard Delivery - Free</p>
+            <div class="total">
+                <h3>Total: </h3>
+                <div class="total-price">
+                    <p class="price-sum">{{ $cartPrice }}€</p>
+                </div>
+            </div>
+            <div class="checkout-button column">
+                <a href="{{ route('checkout') }}" class="column"><button>Checkout</button></a>
             </div>
         </div>
-        <div class="promo column justify-center gap-1">
-            <h3><label for="promo-code">
-                    Promo code:
-                </label></h3>
-            <div class="apply-button">
-                <input type="text" id="promo-code" name="promo-code">
-                <button>Apply</button>
-            </div>
-        </div>
-        <span class="line"></span>
-        @foreach ($appliedVouchers as $voucher)
-            @php
-                $difference = $voucher->getProduct->price - $voucher->bargainMessage->proposed_price;
-            @endphp
-            <div class="voucher row justify-between">
-                <p class="voucher-code">Voucher: <span>{{$voucher->code}}</span><a href="#" class="voucher-remove" data-voucher-code="{{$voucher->code}}"><ion-icon name="close-circle-outline"></ion-icon></a></p>
-                <p class="difference">-{{$difference}}€</p>
-            </div>
-        @endforeach
-        <div class="total">
-            <h3>Total: </h3>
-            <div class="total-price">
-                <p>{{$cartPrice}}€</p>
-            </div>
-        </div>
-        <div class="checkout-button column">
-            <a href="{{ route('checkout') }}" class="column"><button>Checkout</button></a>
-        </div>
-    </div>
-</section>
-
-
+    </section>
 @endsection
